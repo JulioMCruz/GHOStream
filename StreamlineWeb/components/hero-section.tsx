c759011beb61151b2b02a44f4731ddcfb61c3ea2
Harshal Bhangale <buddyharshal2751@gmail.com>
2024-01-21 14:58:35 +0530

    landing updates

--- StreamlineWeb/components/hero-section.tsx
+++ StreamlineWeb/components/hero-section.tsx
@@ -1,20 +1,29 @@
-"use client"
-
 import { Button } from "./ui/button"
-
+import Image from 'next/image';
 
 export default function HeroSection() {
-
   return (
     <div className=" pt-[20px] md:pt-[50px] lg:pt-[100px] pb-[5px] md:pb-[25px] flex flex-col items-center justify-center text-center px-4">
-        <h1 className="text-4xl font-bold mb-4 leading-snug">Streamline</h1>
+        
+        <Image
+          src="/Group 3.png" // Corrected path
+          width={300}    // Desired width
+          height={150}   // Desired height
+          alt="Description"
+          className="m-3" // Alt text for accessibility
+        />
       <h1 className="text-md md:text-xl lg:text-4xl font-bold mb-4 leading-snug  max-w-4xl">
-      Nulla scelerisque libero sagittis velit aliquet
+      Let your Assets Interest pay for your expenses.
       </h1>
       <p className="mb-2 md:mb-12 text-md md:text-lg lg:text-xl leading-snug max-w-4xl">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget justo sodales, ornare justo sit amet, fringilla augue. Vestibulum dignissim enim at hendrerit fermentum. Quisque ornare libero sed ultricies viverra. Nulla facilisi. Fusce vitae nulla velit. Aenean quis massa eget magna vehicula lacinia. Donec a tortor eget tellus scelerisque vehicula ac ut dolor.
+      Streamline enables to deposit any token, and pay the interest  
       </p>
+      <Image
+          src="/eth.png" // Corrected path
+          width={500}    // Desired width
+          height={300}   // Desired height
+          alt="Description" // Alt text for accessibility
+        />
     </div>
   )
 }
-
